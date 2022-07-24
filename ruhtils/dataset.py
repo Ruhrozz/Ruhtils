7009a83156bf3d69f0652f5546cdc72a7437499d
ruhrozz <Ruhrozz@bk.ru>
2022-07-24 12:07:21 +0700

    Added class Dataset. Removed overriding of DatasetFolder's __getitem__.

--- ruhtils/dataset.py
+++ ruhtils/dataset.py
@@ -1,13 +1,14 @@
 import cv2
 from random import shuffle
-from torchvision.datasets.folder import DatasetFolder
-from typing import Any, Tuple, Optional, Callable, List
 
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import DatasetFolder
+from typing import Tuple, Optional, Callable, List, Any
 
 IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
 
 
-def loader(image_path):
+def default_loader(image_path):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -17,13 +18,18 @@ def loader(image_path):
 class ImageFolder(DatasetFolder):
     def __init__(self,
                  root: str,
+                 use_albumentations: Optional[bool] = False,
                  transform: Optional[Callable] = None,
                  target_transform: Optional[Callable] = None) -> None:
         super().__init__(root=root,
-                         transform=transform,
                          target_transform=target_transform,
                          extensions=IMG_EXTENSIONS,
-                         loader=loader)
+                         loader=default_loader)
+
+        if use_albumentations:
+            self.transform = lambda image: transform(image=image)["image"]
+        else:
+            self.transform = transform
 
     def take_valid(self, sample: float = 0.2) -> List[Tuple[str, int]]:
         """Splits sample to 2 disjoint sets with given validation percentage
@@ -42,14 +48,41 @@ class ImageFolder(DatasetFolder):
 
         return valid
 
+
+class Dataset(VisionDataset):
+    def __init__(
+            self,
+            root: str,
+            samples: List[Tuple[str, int]],
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            use_albumentations: bool = False,
+    ) -> None:
+        super().__init__(root=root)
+
+        if use_albumentations and transform is not None:
+            self.transform = lambda image: transform(image=image)["image"]
+        else:
+            self.transform = transform
+
+        if use_albumentations and target_transform is not None:
+            self.target_transform = lambda image: transform(image=image)["image"]
+        else:
+            self.target_transform = target_transform
+
+        self.samples = samples
+
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         path, target = self.samples[index]
-        sample = self.loader(path)
+        sample = default_loader(path)
 
         if self.transform is not None:
-            sample = self.transform(image=sample)["image"]
+            sample = self.transform(sample)
 
         if self.target_transform is not None:
-            target = self.target_transform(image=target)["image"]
+            target = self.target_transform(target)
 
         return sample, target
+
+    def __len__(self) -> int:
+        return len(self.samples)
