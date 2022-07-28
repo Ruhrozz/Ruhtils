9bae7d698e59709cee04b008626cef4383ba5311
ruhrozz <Ruhrozz@bk.ru>
2022-07-28 09:53:25 +0700

    Added get_dataloader.

--- ruhtils/data/dataloader.py
+++ ruhtils/data/dataloader.py
@@ -1,6 +1,11 @@
-# TODO: get dataloader function
+from torch.utils.data import DataLoader
+
+
 # TODO: get distributed dataloader function
+def get_dataloader(dataset, device, **dataloader_kwargs):
 
+    dataloader = DataLoader(dataset,
+                            pin_memory=True if device == "GPU" else False,
+                            **dataloader_kwargs)
 
-
-
+    return dataloader
