c10e86d9270f5ac97986c770bff9b9c9d6137bf6
ruhrozz <Ruhrozz@bk.ru>
2022-07-28 09:43:42 +0700

    Added valid function.

--- ruhtils/valid.py
+++ ruhtils/valid.py
@@ -1,3 +1,23 @@
-# TODO: valid function
+import torch
+from typing import Tuple
+import numpy as np
 
 
+def valid_fn(model, dataloader, criterion, device) -> Tuple[float, float]:
+    model.eval()
+
+    running_accuracy = 0
+    running_loss = 0
+
+    for batch_idx, (inputs, labels) in enumerate(dataloader):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            predicted = model(inputs)
+
+            loss = criterion(predicted, labels)
+            running_loss += loss
+            running_accuracy += np.equal(labels, predicted.argmax(axis=1)).sum()
+
+    return running_accuracy / len(dataloader.dataset), running_loss / len(dataloader)
