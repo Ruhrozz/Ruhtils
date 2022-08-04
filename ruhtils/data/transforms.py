43ec73ad223ca087808d9da4874a384cdcc98c10
ruhrozz <Ruhrozz@bk.ru>
2022-08-04 23:08:02 +0700

    Added torchvision augmentations.

--- ruhtils/data/transforms.py
+++ ruhtils/data/transforms.py
@@ -5,12 +5,14 @@ further usage for augmentation tasks"""
 from typing import Optional, Callable
 import cv2
 import albumentations as A
-from albumentations.pytorch import transforms as T
+from torchvision import transforms as T
+from albumentations.pytorch import transforms as AT
 
 
 def kbrodt_transforms(is_train: Optional[bool] = False,
                       p: Optional[float] = 0.5) -> Callable:
-    """This function was taken from kbrodt GitHub
+    """Transforms were taken from kbrodt GitHub.
+    Returns Albumentations.
     Args:
         is_train: Optional[bool]
             Return augs for valid or train data.
@@ -63,14 +65,41 @@ def kbrodt_transforms(is_train: Optional[bool] = False,
             A.RandomSunFlare(src_radius=150, p=0.2),
             A.ShiftScaleRotate(border_mode=cv2.BORDER_CONSTANT, p=0.2),
             A.Resize(256, 256),
-            T.ToTensorV2(),
+            AT.ToTensorV2(),
         ]
     else:
         augs = [
             A.Resize(256, 256),
-            T.ToTensorV2(),
+            AT.ToTensorV2(),
         ]
 
-    augs_compose = A.Compose(augs)
+    return A.Compose(augs)
 
-    return augs_compose
+
+def torch_transforms(is_train: Optional[bool] = False,
+                     p: Optional[float] = 0.5) -> Callable:
+    """Function returns a set of torchvision augmentations.
+        Args:
+            is_train: Optional[bool]
+                Return augs for valid or train data.
+            p: Optional[float]
+                Percentage of augmented data.
+        Return:
+            Composition of augmentations for train or valid data.
+        """
+    if is_train:
+        augs = [
+            T.Resize((256, 256)),
+            T.RandomHorizontalFlip(p=p),
+            T.RandomVerticalFlip(p=p),
+            T.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0),
+            T.ToTensor(),
+            T.RandomErasing(p=p),
+        ]
+    else:
+        augs = [
+            T.Resize((256, 256)),
+            T.ToTensor(),
+        ]
+
+    return T.Compose(augs)
