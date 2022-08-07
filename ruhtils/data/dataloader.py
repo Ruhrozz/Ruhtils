ca06f80a1bc6f66b245567de4fb740d7543931a2
ruhrozz <Ruhrozz@bk.ru>
2022-08-07 13:11:26 +0700

    Hotfix.

--- ruhtils/data/dataloader.py
+++ ruhtils/data/dataloader.py
@@ -8,12 +8,12 @@ from torch.utils.data import DataLoader
 
 
 # TODO: get distributed dataloader function
-def get_dataloader(dataset, device: str, **dataloader_kwargs):
+def get_dataloader(dataset, run_on: str, **dataloader_kwargs):
     """ This function makes dataloader from dataset.
     Args:
         dataset:
             Torch dataset
-        device:
+        run_on:
             Any of ["GPU", "CPU", "TPU"]
         **dataloader_kwargs:
             Keyword arguments that will be passed directly
@@ -24,7 +24,7 @@ def get_dataloader(dataset, device: str, **dataloader_kwargs):
     """
 
     dataloader = DataLoader(dataset,
-                            pin_memory=(device == "GPU"),
+                            pin_memory=(run_on == "GPU"),
                             **dataloader_kwargs)
 
     return dataloader
