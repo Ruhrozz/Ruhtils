cfec749aaaa50fe0754579c3fd5339f24c8f53f4
ruhrozz <Ruhrozz@bk.ru>
2022-08-07 13:12:10 +0700

    Merged train and valid into trainval.

--- ruhtils/train.py
+++ ruhtils/train.py
@@ -2,48 +2,71 @@
 
 
 from typing import Tuple
+import torch
 from tqdm import tqdm
 
 
-def train_fn(model, dataloader, optimizer, criterion, **kwargs) -> Tuple[float, float]:
+def _kwargs_check(kwargs):
+    if "epoch" not in kwargs:
+        kwargs["epoch"] = '???'
+    if "is_train" not in kwargs:
+        kwargs["is_train"] = False
+    if kwargs['is_train']:
+        kwargs['desc'] = "Training"
+    else:
+        kwargs['desc'] = "Validating"
+    return kwargs
+
+
+def _valid_fn(model, criterion, inputs, labels):
+    with torch.no_grad():
+        predicted = model(inputs)
+        loss = criterion(predicted, labels)
+        return loss.item(), (labels == predicted.argmax(axis=1)).sum()
+
+
+def _train_fn(model, optimizer, criterion, inputs, labels):
+    optimizer.zero_grad()
+    predicted = model(inputs)
+    loss = criterion(predicted, labels)
+    loss.backward()
+    optimizer.step()
+    return loss.item(), (labels == predicted.argmax(axis=1)).sum()
+
+
+def trainval(model, dataloader, criterion, optimizer=None, **kwargs) -> Tuple[float, float]:
     """Args:
             model
             dataloader
-            optimizer
             criterion
+            optimizer: Optional if valid
             kwargs:
-                device
-                epoch
+                device: torch.device
+                epoch: int
+                is_train: bool
         Return:
             (Accuracy, Loss) -  for history in view module."""
-    model.train()
-    running_loss = 0
     running_accuracy = 0
-    if "epoch" not in kwargs:
-        kwargs["epoch"] = '???'
+    running_loss = 0
+    _kwargs_check(kwargs)
 
     pbar = tqdm(dataloader,
-                desc=f"Epoch: {kwargs['epoch']} --- Training... ",
                 leave=False,
-                unit="image")
+                unit="image",
+                desc=f"Epoch: {kwargs['epoch']} --- {kwargs['desc']}... ")
+    model.train()
+    if not kwargs['is_train']:
+        model.eval()
 
     for inputs, labels in pbar:
         if "device" in kwargs:
             inputs = inputs.to(kwargs["device"])
             labels = labels.to(kwargs["device"])
-
-        optimizer.zero_grad()
-
-        predicted = model(inputs)
-        loss = criterion(predicted, labels)
-
-        running_loss += loss.item()
-        running_accuracy += (labels == predicted.argmax(axis=1)).sum()
-
-        loss.backward()
-
-        optimizer.step()
-
-    model.eval()
+        if kwargs['is_train']:
+            loss, acc = _train_fn(model, optimizer, criterion, inputs, labels)
+        else:
+            loss, acc = _valid_fn(model, criterion, inputs, labels)
+        running_loss += loss
+        running_accuracy += acc
 
     return running_accuracy / len(dataloader.dataset), running_loss / len(dataloader)
