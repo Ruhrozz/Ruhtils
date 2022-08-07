acb469148f0ea91474c7332419b4c6848b8b6fff
ruhrozz <Ruhrozz@bk.ru>
2022-08-07 12:24:35 +0700

    Added albu example. Kbrodt is available now.

--- ruhtils/data/transforms.py
+++ ruhtils/data/transforms.py
@@ -6,12 +6,12 @@ from typing import Optional, Callable
 import cv2
 import albumentations as A
 from torchvision import transforms as T
-from albumentations.pytorch import transforms as AT
+from albumentations.pytorch import ToTensorV2
 
 
 def kbrodt_transforms(is_train: Optional[bool] = False,
                       p: Optional[float] = 0.5) -> Callable:
-    """Transforms were taken from kbrodt GitHub.
+    """Transforms were taken from kbrodt GitHub with adding normalization.
     Returns Albumentations.
     Args:
         is_train: Optional[bool]
@@ -23,6 +23,7 @@ def kbrodt_transforms(is_train: Optional[bool] = False,
     """
     if is_train:
         augs = [
+            A.Resize(256, 256),
             A.HorizontalFlip(p=p),
             A.CoarseDropout(max_holes=8, max_height=8, max_width=8, p=p),
             A.OneOf(
@@ -64,13 +65,14 @@ def kbrodt_transforms(is_train: Optional[bool] = False,
             A.RandomShadow(p=0.2),
             A.RandomSunFlare(src_radius=150, p=0.2),
             A.ShiftScaleRotate(border_mode=cv2.BORDER_CONSTANT, p=0.2),
-            A.Resize(256, 256),
-            AT.ToTensorV2(),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
         ]
     else:
         augs = [
             A.Resize(256, 256),
-            AT.ToTensorV2(),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
         ]
 
     return A.Compose(augs)
@@ -103,3 +105,25 @@ def torch_transforms(is_train: Optional[bool] = False,
         ]
 
     return T.Compose(augs)
+
+
+def albu_transforms(is_train=False):
+    """Example of albumentation augs"""
+    if is_train:
+        augs = [
+            A.SmallestMaxSize(max_size=160),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+            A.RandomCrop(height=128, width=128),
+            A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            A.RandomBrightnessContrast(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    else:
+        augs = [
+            A.SmallestMaxSize(max_size=160),
+            A.CenterCrop(height=128, width=128),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    return A.Compose(augs)
