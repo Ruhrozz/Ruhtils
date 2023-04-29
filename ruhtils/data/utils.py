8eb0069e9da9f4ea41b126e59596ed31ac0d535f
Ivan Malofeev <79824942+Ruhrozz@users.noreply.github.com>
2023-04-29 12:07:10 +0700

    Update utils.py

--- ruhtils/data/utils.py
+++ ruhtils/data/utils.py
@@ -39,36 +39,3 @@ def yolo_show_label(img_p, label_p, size=(10)):
             patch = yolo_bbox(x, y, w, h, img.shape)
             ax.add_patch(patch)
     plt.show()
-    
-for stage in ["test", "train", "valid"]:
-    p = pathlib.Path(f"{stage}/labels/")
-
-    p_to = pathlib.Path(f"{stage}/immed_det_labels")
-    p_to.mkdir(parents=True, exist_ok=True)
-
-    for mask in tqdm(list(p.iterdir())):
-        with open(mask, "r") as file:
-            file = file.read()
-            img = cv2.imread(f"{stage}/images/" + mask.stem + '.jpg')
-
-            # Show img
-            # fig, ax = plt.subplots(figsize=(10, 10))
-            # ax.imshow(img)
-
-            with open(p_to/mask.name, "w") as new_label:
-                for row in file.split('\n'):
-                    row = row.split()
-
-                    cls = int(row[0])
-                    x = np.array(list(map(float, row[1::2]))) * img.shape[1]
-                    y = np.array(list(map(float, row[2::2]))) * img.shape[0]
-
-                    # Show segmentation mask
-                    # plt.plot(x, y)
-
-                    x, y, w, h = get_yolo_xy(x.max(), x.min(), y.max(), y.min(), img.shape)
-
-                    # Show detection bboxes
-                    # yolo_show(x, y, w, h, img.shape, ax)
-
-                    new_label.write(f"{cls} {x} {y} {w} {h}\n")
